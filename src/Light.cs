b1292385780d711b4b1ac484d4cb26f8b41e0cb7
LiamAttClarke <liam.att.clarke@gmail.com>
2025-09-09 09:56:33 -0400

    Modulate light energy with brightness

--- src/Light.cs
+++ src/Light.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace TouchGrass
@@ -7,10 +8,19 @@ namespace TouchGrass
     public partial class Light : Node3D
     {
         [Export] public float Brightness { get; private set; }
+        [Export] public Light3D LightNode { get; set; }
+
+        public override void _EnterTree()
+        {
+            base._EnterTree();
+
+            if (LightNode == null) throw new Exception("No light selected");
+        }
 
         public void SetBrightness(float brightness)
         {
             Brightness = brightness;
+            LightNode.LightEnergy = brightness;
         }
     }
 }
